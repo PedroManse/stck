67dbb83bdbb0d41d161ff69b423008041f1589ef
Manse <pedro.manse@dmk3.com.br>
2025-07-16 18:03:14 -0300

    error variant docs for RuntimeErrorKing

--- src/error.rs
+++ src/error.rs
@@ -269,62 +269,103 @@ pub enum StckError {
 #[cfg_attr(test, derive(PartialEq))]
 #[cfg_attr(not(feature = "exhaustive-errors"), non_exhaustive)]
 pub enum RuntimeErrorKind {
+    /// Tried to execute a user-defined functions but there weren't enough arguments in the stack
     #[error("Not enough arguments to execute {name}, got {got:?} needs {needs:?}")]
     UserFnMissingArgs {
         name: String,
         got: Vec<Value>,
         needs: Vec<String>,
     },
+    /// `!` (aka unwrap) builtin executed on Error
+    ///
+    /// The unwrap builtin works like [`Result::unwrap`],
+    /// on a [`Value::Result`], but an Error variant was found
     #[error("Found {} while executing `!` on a Result: {error}", "Error".bright_yellow())]
     UnwrapResultBuiltinFailed { error: Value },
+    /// `!` (aka unwrap) builtin executed on None
+    ///
+    /// The unwrap builtin works like [`Option::unwrap`],
+    /// on a [`Value::Option`], but a None variant was found
     #[error("Found missing value while exeuting `!` on an Option")]
     UnwrapOptionBuiltinFailed,
+    /// Tried to compare values of incompatible types
     #[error("Can't compare {this} with {that}")]
     Compare { this: Value, that: Value },
+    /// Tried to execute `(switch)` without any value in the stack to be used as the case
     #[error("Switch case with no value")]
     SwitchCaseWithNoValue,
+    /// Tried to format a string with the `%%` builtin but an unknown format directive was used
     #[error(
         "`%%` ({0}) doesn't recognise the format directive `{1}`, only '%', 'd', 's', 'v', 'V' and 'b' are avaliable"
     )]
     UnknownStringFormat(String, char),
+    /// Tried to format a string with `%%` but there weren't enough values in the stack
     #[error("`%%` ({0}) Can't capture any value, the stack is empty")]
     MissingValue(String, char),
+    /// Tried to format a string with `%%` but a value of the wrong type was popped
     #[error("`%%` ({0}) The provided value, {1}, can't be formatted with `{2}`")]
     WrongValueType(String, Value, char),
+    /// Concrete type error
+    ///
+    /// A value was given but a function or closure expected another type
     #[error("Expected type: {0} got value {1}: {ty}", ty=TypeTester::from(.1.as_ref()))]
     Type(TypeTester, Box<Value>),
+    /// Abstract type error
+    ///
+    /// An input or return type of a higher order closure didn't match the expected type from
+    /// another function or closure
     #[error("Expected type: {0} got {1}")]
     TypeType(TypeTester, TypeTester),
+    /// Wrong output count from function
+    ///
+    /// A function returned the wrong amount of values according to it's return argument
+    /// list
     #[error("Output of function `{fn_name}` error, Expected {expected:?} got {got:?}")]
     OutputCount {
         fn_name: String,
         expected: usize,
         got: usize,
     },
+    /// Wrong output count from a closure
+    ///
+    /// A closure returned the wrong amount of values according to it's return argument
+    /// list
     #[error("Output of closure error, Expected {expected:?} got {got:?}")]
     OutputClosureCount { expected: usize, got: usize },
+    /// Failed to find a user-defined function
+    ///
+    /// Different from [`RuntimeErrorKind::MissingIdent`] because this only applies to user-defined
+    /// functions. This error may occour if the `(@<fn name>)` keyword was executed but the
+    /// functions doesn't exist
     #[error("No such user-defined function `{0}`")]
     MissingUserFunction(String),
+    /// A check code block was executed and didn't create exactly one value
+    ///
+    /// Maybe this type will be removed together with the restriction
     #[error("WrongStackSizeDiffOnCheck {old_stack_size} -> {new_stack_size}")]
     WrongStackSizeDiffOnCheck {
         old_stack_size: usize,
         new_stack_size: usize,
         new_should_stack_size: usize,
     },
+    /// A check code block created a non-bool value
     #[error("check blocks must recieve one boolean, recieved {got}")]
     WrongTypeOnCheck { got: Value },
+    /// Not enough values where provided to a builtin
     #[error("Function {for_fn} accepts [{args}]. But {this_arg} is missing")]
     MissingValueForBuiltin {
         for_fn: String,
         args: String,
         this_arg: &'static str,
     },
+    /// Not enough values where provided to a builtin with dynamic capture size
     #[error("Function {for_fn} accepts {args}. But {missing} args are missing")]
     MissingValuesForBuiltin {
         for_fn: String,
         args: &'static str,
         missing: isize,
     },
+    /// A value provided for a builtin was of incorrect type
     #[error(
         "Function {for_fn} accepts {args}. But [{this_arg}] must be a {expected} and got {got}"
     )]
@@ -335,23 +376,35 @@ pub enum RuntimeErrorKind {
         got: Box<Value>,
         expected: &'static str,
     },
+    /// Tried to get an inexistant variable
     #[error("The variable {0} is not defined")]
     NoSuchVariable(String),
+    /// User tried to convert a user function into a closure, but it had zero arguments
     #[error(
         "Can't make function ({fn_name}) that takes no arguments into closure, since that would never be executed"
     )]
     CantMakeFnIntoClosureZeroArgs { fn_name: String },
+    /// User tried to convert a user function into a closure, but it had All Stack Capture
     #[error(
         "Can't make function ({fn_name}) that takes entire stack into closure, since it would never be executed"
     )]
     CantMakeFnIntoClosureAllStack { fn_name: String },
+    /// User tried to create a closure, but it had zero arguments
     #[error("Can't make closure with zero arguments, it's code spans these bytes: {span:?}")]
     CantInstanceClosureZeroArgs { span: Range<usize> },
+    /// DEVERROR, this error should never be triggered
+    ///
+    /// Tried to [`Closure::fill`] already full closure
     #[error(
         "Closure's arguments ({:?}) are filled, but still tried to add more",
         closure_args
     )]
     DEVFillFullClosure { closure_args: ClosurePartialArgs },
+    /// DEVERROR, this error should never be triggered
+    ///
+    /// Tried to Immediate a closure that already had parent-scope arguments.
+    ///
+    /// This error could be removed with an enum for closures with and without scope arguments.
     #[error(
         "Closure's arguments ({closure_args:?})'s parent function values are beeing reset with {parent_args:?}"
     )]
@@ -359,6 +412,10 @@ pub enum RuntimeErrorKind {
         closure_args: Box<ClosurePartialArgs>,
         parent_args: HashMap<ArgName, FnArg>,
     },
+    /// DEVERROR, this error should never be triggered
+    ///
+    /// Internl user structure workings resolved a field's index into an out-of-bounds index for
+    /// the field list of the structure
     #[error(
         "DEV ERROR, this error should never appear to you:\nThe struct {0}, with method {1:?}, tried using field with index {2}; {3}"
     )]
@@ -368,18 +425,25 @@ pub enum RuntimeErrorKind {
         usize,
         Box<UserStructInstance>,
     ),
+    /// User tried to execute a function, builtin or hook that doesn't exist
     #[error("No such function or function argument called `{0}`")]
     MissingIdent(String),
+    /// User requires a module that isn't loaded
     #[error("Module `{0}` is required but was not loaded")]
     MissingModule(String),
+    /// User tried to execute an action on/about a structure, but the method doesn't exist
     #[error("Method {0}'s fieldless action doesn't exist")]
     NoSuchFieldlessAction(String),
+    /// User tried to access a field that doesn't exist
     #[error("Method {0}'s field doesn't exist")]
     NoSuchField(String),
+    /// User tried to use a structure action that doesn't exist
     #[error("Method {0}'s action doesn't exist")]
     NoSuchAction(String),
+    /// User tried to create a structure but there weren't enough values in the stack
     #[error("Not enough arguments to make {0}")]
     NotEnoughArgsForNew(Rc<UserStructDef>),
+    /// User tried to execute a method but the type of the provided value was wrong
     #[error("Wrong value, {0}, for type {1}, while doing {2:?} on {3}")]
     WrongTypeForMethod(
         Box<Value>,
@@ -387,6 +451,8 @@ pub enum RuntimeErrorKind {
         UserStructMethod,
         Rc<UserStructDef>,
     ),
+    // TODO: better docs or overall option, maybe #217
+    /// User tried to execute an action disabled
     #[error("Tried to execute dissalowed action: {0}")]
     DisallowedAction(UnauthorizedAction),
 }
